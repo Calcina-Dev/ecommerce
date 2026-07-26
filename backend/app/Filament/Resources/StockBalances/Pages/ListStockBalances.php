b9c634f148afb79cba58e9bc22d3b38d042b35f3
Carlos Mendoza <carlos200995321@gmail.com>
2026-07-26 06:08:37 -0500

    fix: remove manual creation of StockBalance in Filament

--- backend/app/Filament/Resources/StockBalances/Pages/ListStockBalances.php
+++ backend/app/Filament/Resources/StockBalances/Pages/ListStockBalances.php
@@ -13,7 +13,7 @@ class ListStockBalances extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            CreateAction::make(),
+            // CreateAction::make(),
         ];
     }
 }
